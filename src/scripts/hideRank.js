daaf30a6cb05674c37617b9d1b5363f3f8e12318
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-12-27 22:06:29 -0300

    adding project base

---
+++ src/scripts/hideRank.js
@@ -0,0 +1,3 @@
+export default function hideRank() {
+    return 'sera'
+}
---
+++ webpack.config.dev.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/scripts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'script.min.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /.(js)$/,
+                exclude: /[\/]node_modules[\/]/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            }
+        ]
+    },
+
+
+}
